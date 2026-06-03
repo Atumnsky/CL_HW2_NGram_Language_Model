43ee2825078cabd8ae450201a1b9d6d3fd0576af
Aki_Sora <yuepeng0319@gmail.com>
2026-06-03 17:15:26 +0200

    12 N-gram models from train.zip checked

--- demo.py
+++ demo.py
@@ -16,6 +16,9 @@ tok_nltk = corpus_reader.TokenizerNLTK()
 reader_plain = corpus_reader.CorpusReader(train_path, tok_plain)
 reader_nltk = corpus_reader.CorpusReader(train_path, tok_nltk)
 
+"""
+12 N-gram models from train.zip
+"""
 for n in [1,2,3]:
     for mode in ["sentence", "word"]:
         for tok, reader in [(tok_plain, reader_plain),(tok_nltk, reader_nltk)]:
@@ -27,4 +30,8 @@ for n in [1,2,3]:
             ngram_model = model.NGramModel(data, n, tok, mode = mode)
 
             generate.demonstrate(ngram_model)
-            
+
+
+"""
+12 N-gram models from gutenberg
+"""
